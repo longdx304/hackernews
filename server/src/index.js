56022c1e64db641ebf528aa33a873d32a98105ab
Long Do <longdo@dobtech.vn>
2019-11-12 18:42:29 +0700

    updated app to use Prisma and MongoDB for CRUD

--- server/src/index.js
+++ server/src/index.js
@@ -1,70 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client');
 
-async function main() {
-    // Create new link
-    const newLink = await prisma.createLink({
-        url: "www.prisma.io",
-        description: "Prisma replaces traditional ORMs"
-    });
-    console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
-
-    // Read all links from database
-    const allLinks = await prisma.links();
-    console.log(allLinks);
-}
-main().catch(e => console.error(e));
-
-// Dummy data
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
-
 // Define implementation of Schema
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (parent, args) => {
-            return links.find((link) => link.id === args.id);
+        feed: (root, args, context, info) => {
+            return context.prisma.links()
+        },
+        link: (root, args, context) => {
+            return context.prisma.link({id: args.id});
         }
     },
     Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link);
-            return link;
+        post: (root, args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
+                description: args.description
+            });
         },
-        updateLink: (parent, args) => {
-            // Find link with args id
-            const linkIndex = links.findIndex(link => link.id === args.id);
-            // Replace link with new info
-            if(args.url) {
-                links[linkIndex].url = args.url;
-            }
-            if(args.description) {
-                links[linkIndex].description = args.description;
-            }
-            
-            return links[linkIndex];
+        updateLink: (root, args, context) => {
+            return context.prisma.updateLink({
+                where: {id: args.id},
+                data: {
+                    url: args.url,
+                    description: args.description
+                }
+            });
         },
-        deleteLink: (parent, args) => {
-            // Find link with args id
-            const linkIndex = links.findIndex(link => link.id === args.id);
-            const removedLink = links[linkIndex];
-            links.splice(linkIndex, 1);
-            return removedLink;
-            /* const resultLinks = links.filter(link => link.id !== args.id);
-            links = resultLinks;
-            return links; */
+        deleteLink: (root, args, context) => {
+            return context.prisma.deleteLink({
+                id: args.id
+            });
         }
     }
 }
@@ -73,7 +40,6 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
-
-
+    context: { prisma }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
