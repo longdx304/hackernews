0bb4238ae30dc6c9f931a2446ec70abd2365f58f
Long Do <longdo@dobtech.vn>
2019-11-12 18:01:37 +0700

    add Prisma and MongoDB

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client');
+
+async function main() {
+    // Create new link
+    const newLink = await prisma.createLink({
+        url: "www.prisma.io",
+        description: "Prisma replaces traditional ORMs"
+    });
+    console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
+
+    // Read all links from database
+    const allLinks = await prisma.links();
+    console.log(allLinks);
+}
+main().catch(e => console.error(e));
 
 // Dummy data
 let links = [{
